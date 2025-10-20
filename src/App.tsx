88af1b02228334e7f9a9007ab88997018c7de434
jaehun choi <99630193+MrNubi@users.noreply.github.com>
2025-10-20 11:25:02 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import ExcelInsertPage from "./pages/ExelInsertPage";
 import NewCompaniesPage from "./pages/NewCompaniesPage";
-
+//123
 export default function App() {
 
   return (
